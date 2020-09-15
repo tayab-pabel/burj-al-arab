e8ce2132fc9bba3851ee321ec6722e3dd2bb4924
tayab-pabel <tayabpabel@yahoo.com>
2020-09-15 20:21:15 +0600

    first commit

---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDnHiLt6lO8XHC3HlUmRxuFlOzJkv9fqeU",
+    authDomain: "burj-al-arab-cd790.firebaseapp.com",
+    databaseURL: "https://burj-al-arab-cd790.firebaseio.com",
+    projectId: "burj-al-arab-cd790",
+    storageBucket: "burj-al-arab-cd790.appspot.com",
+    messagingSenderId: "311056638889",
+    appId: "1:311056638889:web:b860871925f8edf57cdab8"
+  };
+
+  export default firebaseConfig;
